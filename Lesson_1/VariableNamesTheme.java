9fcfd5ee8b06c45e0ad4349125b8e70e98ab1d17
arkbazh <bzhrk@icloud.com>
2025-03-24 16:15:12 +0400

    Initial commit: добавлены Lesson_1 и .gitignore

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,150 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        // 1. Разные переменные
+        // Цифра
+        int num = 1;
+        // Сумма чисел
+        int numSum = 1;
+        // Произведение чисел
+        int numMult = 1;
+        // Максимальное число
+        int maxNum = 1;
+        // Количество десятков
+        int numberTens = 1;
+        // Вес собаки
+        int dogWeight = 1;
+        // Исходное число
+        int originalNum = 1;
+        // Процент по вкладу
+        int depositInterest = 1;
+        // Переменная хранит символ &
+        char amp = '&';
+        // Код ошибки
+        String errCode = "403";
+        // Тип сообщения
+        String msgType = "type";
+        // Число нулей
+        int zeroNum = 1;
+        // Уникальное число
+        int uniqueNum = 1;
+        // Случайное число
+        int randomNum = 1;
+        // Математическое выражение
+        int mathExpr = 1;
+        // Счет в игре
+        int gameScore = 1;
+        // Максимальная длина
+        int maxLength = 1;
+        // Пункт меню
+        String menuItem = "item";
+        // Стоимость кофе
+        int coffeeCost = 1;
+        // Начальная дата
+        int startDate = 1;
+        // Окончание диапазона
+        int rangeEnd = 1;
+        // Имя работника месяца
+        String employeeOfMonthName = "Name";
+        // Название электронной книги
+        String ebookTitle = "Title";
+        // Размер
+        int size = 1;
+        // Вместимость
+        int capacity = 1;
+        // Счетчик
+        int counter = 1;
+        // Путь до файла
+        String filePath = "path";
+        // Количество чисел в каждой строке
+        int inEachLineNumbersNum = 1;
+
+        // 2. Boolean-переменные
+        // Сотни равны?
+        boolean isEqualsHundreds = true;
+        // Компьютер включен?
+        boolean isComputerOn = true;
+        // Есть равные цифры?
+        boolean hasEqualNum = true;
+        // Создано?
+        boolean isCreated = true;
+        // Пустое?
+        boolean isEmpty = true;
+        // Активно?
+        boolean isActive = true;
+        // Новое?
+        boolean isNew = true;
+        // Электронная почта действительная?
+        boolean isValidEmail = true;
+        // Имеются уникальные строки?
+        boolean hasUniqueLines = true;
+
+        // Аббревиатуры
+        // Старый universally unique identifier
+        String oldUuid = "ID";
+        // Производитель оперативной памяти
+        String ramManufacturer = "Manufacturer";
+        // Емкость жесткого диска
+        int hddCapacity = 1;
+        // Протокол передачи гипертекста
+        String httpProtocol = "http";
+        // Cокращенный uniform resource locator
+        String url = "url";
+        // Новый идентификатор клиента
+        int newClientId = 1;
+        // American standard code for information interchange
+        String acsii = "acsii";
+
+        System.out.println("1. Разные переменные");
+        System.out.println(num);
+        System.out.println(numSum);
+        System.out.println(numMult);
+        System.out.println(maxNum);
+        System.out.println(numberTens);
+        System.out.println(dogWeight);
+        System.out.println(originalNum);
+        System.out.println(depositInterest);
+        System.out.println(amp);
+        System.out.println(errCode);
+        System.out.println(msgType);
+        System.out.println(zeroNum);
+        System.out.println(uniqueNum);
+        System.out.println(randomNum);
+        System.out.println(mathExpr);
+        System.out.println(gameScore);
+        System.out.println(maxLength);
+        System.out.println(menuItem);
+        System.out.println(coffeeCost);
+        System.out.println(startDate);
+        System.out.println(rangeEnd);
+        System.out.println(employeeOfMonthName);
+        System.out.println(ebookTitle);
+        System.out.println(size);
+        System.out.println(capacity);
+        System.out.println(counter);
+        System.out.println(filePath);
+        System.out.println(filePath);
+        System.out.println(inEachLineNumbersNum);
+        System.out.println();
+
+        System.out.println("2. Boolean-переменные");
+        System.out.println(isEqualsHundreds);
+        System.out.println(isComputerOn);
+        System.out.println(hasEqualNum);
+        System.out.println(isCreated);
+        System.out.println(isEmpty);
+        System.out.println(isActive);
+        System.out.println(isNew);
+        System.out.println(isValidEmail);
+        System.out.println(hasUniqueLines);
+        System.out.println();
+
+        System.out.println("3. Аббревиатуры");
+        System.out.println(oldUuid);
+        System.out.println(ramManufacturer);
+        System.out.println(hddCapacity);
+        System.out.println(httpProtocol);
+        System.out.println(url);
+        System.out.println(newClientId);
+        System.out.println(acsii);
+    }
+}
