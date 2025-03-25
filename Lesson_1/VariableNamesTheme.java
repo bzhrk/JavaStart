75543a4fee8ca3f02ffa384570d3b44af4fe86cd
arkbazh <bzhrk@icloud.com>
2025-03-25 10:00:58 +0400

    Обновил переменные в VariableNamesTheme.java

--- Lesson_1/VariableNamesTheme.java
+++ Lesson_1/VariableNamesTheme.java
@@ -1,150 +1,191 @@
 public class VariableNamesTheme {
     public static void main(String[] args) {
         // 1. Разные переменные
+        System.out.println("1. Разные переменные");
+
         // Цифра
         int num = 1;
+        System.out.println(num);
+
         // Сумма чисел
         int numSum = 1;
+        System.out.println(numSum);
+
         // Произведение чисел
         int numMult = 1;
+        System.out.println(numMult);
+
         // Максимальное число
         int maxNum = 1;
+        System.out.println(maxNum);
+
         // Количество десятков
         int numberTens = 1;
+        System.out.println(numberTens);
+
         // Вес собаки
         int dogWeight = 1;
+        System.out.println(dogWeight);
+
         // Исходное число
         int originalNum = 1;
+        System.out.println(originalNum);
+
         // Процент по вкладу
         int depositInterest = 1;
+        System.out.println(depositInterest);
+
         // Переменная хранит символ &
         char amp = '&';
+        System.out.println(amp);
+
         // Код ошибки
         String errCode = "403";
+        System.out.println(errCode);
+
         // Тип сообщения
         String msgType = "type";
+        System.out.println(msgType);
+
         // Число нулей
         int zeroNum = 1;
+        System.out.println(zeroNum);
+
         // Уникальное число
         int uniqueNum = 1;
+        System.out.println(uniqueNum);
+
         // Случайное число
         int randomNum = 1;
+        System.out.println(randomNum);
+
         // Математическое выражение
         int mathExpr = 1;
+        System.out.println(mathExpr);
+
         // Счет в игре
         int gameScore = 1;
+        System.out.println(gameScore);
+
         // Максимальная длина
         int maxLength = 1;
+        System.out.println(maxLength);
+
         // Пункт меню
         String menuItem = "item";
+        System.out.println(menuItem);
+
         // Стоимость кофе
         int coffeeCost = 1;
+        System.out.println(coffeeCost);
+
         // Начальная дата
         int startDate = 1;
+        System.out.println(startDate);
+
         // Окончание диапазона
         int rangeEnd = 1;
+        System.out.println(rangeEnd);
+
         // Имя работника месяца
         String employeeOfMonthName = "Name";
+        System.out.println(employeeOfMonthName);
+
         // Название электронной книги
         String ebookTitle = "Title";
+        System.out.println(ebookTitle);
+
         // Размер
         int size = 1;
+        System.out.println(size);
+
         // Вместимость
         int capacity = 1;
+        System.out.println(capacity);
+
         // Счетчик
         int counter = 1;
+        System.out.println(counter);
+
         // Путь до файла
         String filePath = "path";
+        System.out.println(filePath);
+
         // Количество чисел в каждой строке
         int inEachLineNumbersNum = 1;
-
-        // 2. Boolean-переменные
-        // Сотни равны?
-        boolean isEqualsHundreds = true;
-        // Компьютер включен?
-        boolean isComputerOn = true;
-        // Есть равные цифры?
-        boolean hasEqualNum = true;
-        // Создано?
-        boolean isCreated = true;
-        // Пустое?
-        boolean isEmpty = true;
-        // Активно?
-        boolean isActive = true;
-        // Новое?
-        boolean isNew = true;
-        // Электронная почта действительная?
-        boolean isValidEmail = true;
-        // Имеются уникальные строки?
-        boolean hasUniqueLines = true;
-
-        // Аббревиатуры
-        // Старый universally unique identifier
-        String oldUuid = "ID";
-        // Производитель оперативной памяти
-        String ramManufacturer = "Manufacturer";
-        // Емкость жесткого диска
-        int hddCapacity = 1;
-        // Протокол передачи гипертекста
-        String httpProtocol = "http";
-        // Cокращенный uniform resource locator
-        String url = "url";
-        // Новый идентификатор клиента
-        int newClientId = 1;
-        // American standard code for information interchange
-        String acsii = "acsii";
-
-        System.out.println("1. Разные переменные");
-        System.out.println(num);
-        System.out.println(numSum);
-        System.out.println(numMult);
-        System.out.println(maxNum);
-        System.out.println(numberTens);
-        System.out.println(dogWeight);
-        System.out.println(originalNum);
-        System.out.println(depositInterest);
-        System.out.println(amp);
-        System.out.println(errCode);
-        System.out.println(msgType);
-        System.out.println(zeroNum);
-        System.out.println(uniqueNum);
-        System.out.println(randomNum);
-        System.out.println(mathExpr);
-        System.out.println(gameScore);
-        System.out.println(maxLength);
-        System.out.println(menuItem);
-        System.out.println(coffeeCost);
-        System.out.println(startDate);
-        System.out.println(rangeEnd);
-        System.out.println(employeeOfMonthName);
-        System.out.println(ebookTitle);
-        System.out.println(size);
-        System.out.println(capacity);
-        System.out.println(counter);
-        System.out.println(filePath);
-        System.out.println(filePath);
         System.out.println(inEachLineNumbersNum);
         System.out.println();
 
+        // 2. Boolean-переменные
         System.out.println("2. Boolean-переменные");
+
+        // Сотни равны?
+        boolean isEqualsHundreds = true;
         System.out.println(isEqualsHundreds);
+
+        // Компьютер включен?
+        boolean isComputerOn = true;
         System.out.println(isComputerOn);
+
+        // Есть равные цифры?
+        boolean hasEqualNum = true;
         System.out.println(hasEqualNum);
+
+        // Создано?
+        boolean isCreated = true;
         System.out.println(isCreated);
+
+        // Пустое?
+        boolean isEmpty = true;
         System.out.println(isEmpty);
+
+        // Активно?
+        boolean isActive = true;
         System.out.println(isActive);
+
+        // Новое?
+        boolean isNew = true;
         System.out.println(isNew);
+
+        // Электронная почта действительная?
+        boolean isValidEmail = true;
         System.out.println(isValidEmail);
+
+        // Имеются уникальные строки?
+        boolean hasUniqueLines = true;
         System.out.println(hasUniqueLines);
         System.out.println();
 
+        // Аббревиатуры
         System.out.println("3. Аббревиатуры");
+
+        // Старый universally unique identifier
+        String oldUuid = "ID";
         System.out.println(oldUuid);
+
+        // Производитель оперативной памяти
+        String ramManufacturer = "Manufacturer";
         System.out.println(ramManufacturer);
+
+        // Емкость жесткого диска
+        int hddCapacity = 1;
         System.out.println(hddCapacity);
+
+        // Протокол передачи гипертекста
+        String httpProtocol = "http";
         System.out.println(httpProtocol);
-        System.out.println(url);
-        System.out.println(newClientId);
+
+        // Cокращенный uniform resource locator
+        String shortUrl = "url";
+        System.out.println(shortUrl);
+
+        // Новый идентификатор клиента
+        int newCid = 1;
+        System.out.println(newCid);
+
+        // American standard code for information interchange
+        String acsii = "acsii";
         System.out.println(acsii);
+
     }
 }
