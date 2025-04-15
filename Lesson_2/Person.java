ebf652e8f2bcac8d66cb4d31d18879fc9e411f72
arkbazh <bzhrk@icloud.com>
2025-04-15 17:16:48 +0400

    Добавил Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "male";
+    String name = "Tom";
+    int height = 180;
+    int weight = 80;
+    int age = 30;
+
+    void move() {
+        System.out.println("move");
+    }
+
+    void sit() {
+        System.out.println("siting");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    String speack() {
+        return "Hello";
+    }
+
+    void learnJava() {
+        System.out.println("learning");
+    }
+}
